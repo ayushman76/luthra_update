b04347dc00cf84a4246f210f2608b29b9878138d
ayushman76 <78724097+ayushman76@users.noreply.github.com>
2021-03-13 18:49:40 +0530

    Add files via upload

---
+++ script/custom.js
@@ -0,0 +1,12 @@
+function menu(){
+	var nav = document.querySelector('.nav_item');
+    if(nav.style.display==="none"){
+	     nav.style.display="block";
+     }
+	 else{
+		 nav.style.display="none";
+	 }
+}
+
+	
+    
